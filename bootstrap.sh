e921c1ce52ba2481e1e4d5aa4dca55acc6b891f2
Andre Alves <andremralves@gmail.com>
2022-07-10 03:52:28 -0300

    add auto bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+config=$HOME/.config
+
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+echo "";
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # Remove files
+    rm -rf $config/nvim
+    rm -rf $config/alacritty
+    rm -rf $HOME/.tmux.conf
+
+    # Create symbolic links
+    ln --symbolic -v $(pwd)/.config/nvim $config/nvim
+    ln --symbolic -v $(pwd)/.config/alacritty $config/alacritty
+    ln --symbolic -v $(pwd)/.tmux.conf $HOME/.tmux.conf
+fi;
+
